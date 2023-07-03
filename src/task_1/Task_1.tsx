eb2989f7c535a032a3f0da37db4975c205f02a80
IgorBosin <ibosin@yandex.ru>
2023-07-04 00:29:24 +0700

    additional lesson. Wednesday. #2.1 useCallback, React.memo

---
+++ src/task_1/Task_1.tsx
@@ -0,0 +1,37 @@
+import {ChangeEvent, FC, memo, useCallback, useState} from 'react';
+
+const CONTAINER_STYLES = {display: 'flex', flexDirection: 'column', gap: 10};
+
+
+// Task 1
+// If change value on input Title component not re-render
+// If change value on input Task_1 component not re-render
+
+export const Task_1 = () => {
+    console.log('Task_1')
+
+    return (
+        <div style={{...CONTAINER_STYLES} as any}>
+            <Input/>
+            <Title title="I am a title"/>
+        </div>
+    );
+};
+
+const Title = memo((props: { title: string }) => {
+    console.log('Title')
+    return <h3>{props.title}</h3>
+});
+
+const Input: FC = memo(() => {
+
+    const [value, setValue] = useState('');
+
+    const handleChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+        setValue(e.currentTarget.value);
+    }, [setValue])
+
+    return (
+        <input type="text" placeholder="Placeholder" onChange={handleChange}/>
+    );
+})
